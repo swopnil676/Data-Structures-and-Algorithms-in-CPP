6095887644a9843f5f66617d8e56e09b4102052f
Swopnil Biswas <swopnilbiswas437@gmail.com>
2026-08-20 12:48:21 +0530

    Unary Operator Examples

---
+++ 3_Unary_oparators/decre.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=10;
+    
+    // int b=--a; // update ; kaam.
+    // cout<<b<<endl; //9
+    // cout<<a<<endl; //9
+
+    int b=a--; // kaam ; update.
+    cout<<b<<endl; //10
+    cout<<a<<endl; //9
+    return 0;
+}
---
+++ 3_Unary_oparators/incre.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int a=10;
+
+    int b=++a; // update ; kaam.
+    cout<<b<<endl; //11
+    cout<<a<<endl; //11
+    return 0;
+
+    // int b=a++; // kaam ; update.
+    // cout<<b<<endl; //10
+    // cout<<a<<endl; //11
+}
